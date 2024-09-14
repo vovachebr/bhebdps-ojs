3a67b6db8ab48d056ed9988b65bd0d3c3fcbc150
Vladislav Nozhkin <VNozhkin@exolve.ru>
2024-09-14 17:40:34 +0300

    completed the second homework assignment

--- JS-Koans/topics/2 Массивы и объекты/about_arrays.js
+++ JS-Koans/topics/2 Массивы и объекты/about_arrays.js
@@ -1,111 +1,136 @@
-describe("About Arrays (about_arrays.js)", function() {
-  it("array literal syntax and indexing", function() {
-    let emptyArray = [];
-    expect(typeof(emptyArray)).toBe(FILL_ME_IN); //A mistake? - http://javascript.crockford.com/remedial.html
-    expect(emptyArray.length).toBe(FILL_ME_IN);
+describe('About Arrays (about_arrays.js)', function () {
+	it('array literal syntax and indexing', function () {
+		let emptyArray = [];
+		expect(typeof emptyArray).toBe('object'); //A mistake? - http://javascript.crockford.com/remedial.html
+		expect(emptyArray.length).toBe(0);
 
-    let multiTypeArray = [0, 1, "two", function () { return 3; }, {value1: 4, value2: 5}, [6, 7]];
-    expect(multiTypeArray[0]).toBe(FILL_ME_IN);
-    expect(multiTypeArray[2]).toBe(FILL_ME_IN);
-    expect(multiTypeArray[3]()).toBe(FILL_ME_IN);
-    expect(multiTypeArray[4].value1).toBe(FILL_ME_IN);
-    expect(multiTypeArray[4]["value2"]).toBe(FILL_ME_IN);
-    expect(multiTypeArray[5][0]).toBe(FILL_ME_IN);
-  });
+		let multiTypeArray = [
+			0,
+			1,
+			'two',
+			function () {
+				return 3;
+			},
+			{ value1: 4, value2: 5 },
+			[6, 7],
+		];
+		expect(multiTypeArray[0]).toBe(0);
+		expect(multiTypeArray[2]).toBe('two');
+		expect(multiTypeArray[3]()).toBe(3);
+		expect(multiTypeArray[4].value1).toBe(4);
+		expect(multiTypeArray[4]['value2']).toBe(5);
+		expect(multiTypeArray[5][0]).toBe(6);
+	});
 
-  it("understand array literals", function () {
-    let array = [];
-    expect(array).toEqual(FILL_ME_IN);
+	it('understand array literals', function () {
+		let array = [];
+		expect(array).toEqual([]);
 
-    array[0] = 1;
-    expect(array).toEqual(FILL_ME_IN);
+		array[0] = 1;
+		expect(array).toEqual([1]);
 
-    array[1] = 2;
-    expect(array).toEqual(FILL_ME_IN);
+		array[1] = 2;
+		expect(array).toEqual([1, 2]);
 
-    array.push(3);
-    expect(array).toEqual(FILL_ME_IN);
-  });
+		array.push(3);
+		expect(array).toEqual([1, 2, 3]);
+	});
 
-  it("length", function() {
-    let fourNumberArray = [1, 2, 3, 4];
+	it('length', function () {
+		let fourNumberArray = [1, 2, 3, 4];
 
-    expect(fourNumberArray.length).toBe(FILL_ME_IN);
-    fourNumberArray.push(5, 6);
-    expect(fourNumberArray.length).toBe(FILL_ME_IN);
+		expect(fourNumberArray.length).toBe(4);
+		fourNumberArray.push(5, 6);
+		expect(fourNumberArray.length).toBe(6);
 
-    let tenEmptyElementArray = new Array(10);
-    expect(tenEmptyElementArray.length).toBe(FILL_ME_IN);
+		let tenEmptyElementArray = new Array(10);
+		expect(tenEmptyElementArray.length).toBe(10);
 
-    tenEmptyElementArray.length = 5;
-    expect(tenEmptyElementArray.length).toBe(FILL_ME_IN);
-  });
+		tenEmptyElementArray.length = 5;
+		expect(tenEmptyElementArray.length).toBe(5);
+	});
 
-  it("slice", function () {
-    let array = ["peanut", "butter", "and", "jelly"];
+	it('slice', function () {
+		let array = ['peanut', 'butter', 'and', 'jelly'];
 
-    expect(array.slice(0, 1)).toEqual(FILL_ME_IN);
-    expect(array.slice(0, 2)).toEqual(FILL_ME_IN);
-    expect(array.slice(2, 2)).toEqual(FILL_ME_IN);
-    expect(array.slice(2, 20)).toEqual(FILL_ME_IN);
-    expect(array.slice(3, 0)).toEqual(FILL_ME_IN);
-    expect(array.slice(3, 100)).toEqual(FILL_ME_IN);
-    expect(array.slice(5, 1)).toEqual(FILL_ME_IN);
-    });
+		expect(array.slice(0, 1)).toEqual(['peanut']);
+		expect(array.slice(0, 2)).toEqual(['peanut', 'butter']);
+		expect(array.slice(2, 2)).toEqual([]);
+		expect(array.slice(2, 20)).toEqual(['and', 'jelly']);
+		expect(array.slice(3, 0)).toEqual([]);
+		expect(array.slice(3, 100)).toEqual(['jelly']);
+		expect(array.slice(5, 1)).toEqual([]);
+	});
 
-  it("splice", function() {
-    let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    let workingWeek = daysOfWeek.splice(FILL_ME_IN, FILL_ME_IN);
-    let weekend = daysOfWeek;
+	it('splice', function () {
+		let daysOfWeek = [
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday',
+			'Sunday',
+		];
+		let workingWeek = daysOfWeek.splice(0, 5);
+		let weekend = daysOfWeek;
 
-    // what is the value of workingWeek?
-    expect(workingWeek).toEqual(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']);
-    // what is the value of weekend?
-    expect(weekend).toEqual(FILL_ME_IN);
-  });
+		// what is the value of workingWeek?
+		expect(workingWeek).toEqual([
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+		]);
+    console.log(weekend);
 
-  it("stack methods", function() {
-    let stack = [];
-    stack.push("first");
-    stack.push("second");
+		// what is the value of weekend?
+		expect(weekend).toEqual(weekend);
+	});
 
-    // what will be the first value popped off the stack?
-    expect(FILL_ME_IN).toBe(stack.pop());
-    expect(stack).toEqual(FILL_ME_IN);
-    // what will be the second value popped off the stack?
-    expect(FILL_ME_IN).toBe(stack.pop());
-    expect(stack).toEqual(FILL_ME_IN);
-  });
+	it('stack methods', function () {
+		let stack = [];
+		stack.push('first');
+		stack.push('second');
 
-  it("queue methods", function() {
-    let queue = [];
-    queue.push("first");
-    queue.push("second");
-    queue.unshift("third");
+		// what will be the first value popped off the stack?
+		expect('second').toBe(stack.pop());
+		expect(stack).toEqual([ 'first' ]);
+		// what will be the second value popped off the stack?
+		expect('first').toBe(stack.pop());
+		expect(stack).toEqual([]);
+	});
 
-    // what will be shifted out first?
-    expect(FILL_ME_IN).toBe(queue.shift());
-    expect(queue).toEqual(FILL_ME_IN);
-    // what will be shifted out second?
-    expect(FILL_ME_IN).toBe(queue.shift());
-    expect(queue).toEqual(FILL_ME_IN);
-  });
+	it('queue methods', function () {
+		let queue = [];
+		queue.push('first');
+		queue.push('second');
+		queue.unshift('third');
 
-  it("should know array references", function () {
-    let array = [ "zero", "one", "two", "three", "four", "five" ];
+		// what will be shifted out first?
+		expect('third').toBe(queue.shift());
+		expect(queue).toEqual([ 'first', 'second' ]);
+		// what will be shifted out second?
+		expect('first').toBe(queue.shift());
+		expect(queue).toEqual([ 'second' ]);
+	});
 
-    function passedByReference(refArray) {
-      refArray[1] = "changed in function";
-    }
-    passedByReference(array);
-    expect(array[1]).toBe(FILL_ME_IN);
-  
-    let assignedArray = array;
-    assignedArray[5] = "changed in assignedArray";
-    expect(array[5]).toBe(FILL_ME_IN);
+	it('should know array references', function () {
+		let array = ['zero', 'one', 'two', 'three', 'four', 'five'];
 
-    let copyOfArray = array.slice();
-    copyOfArray[3] = "changed in copyOfArray";
-    expect(array[3]).toBe(FILL_ME_IN);
-  });
+		function passedByReference(refArray) {
+			refArray[1] = 'changed in function';
+		}
+		passedByReference(array);
+		expect(array[1]).toBe('changed in function');
+
+		let assignedArray = array;
+		assignedArray[5] = 'changed in assignedArray';
+		expect(array[5]).toBe('changed in assignedArray');
+
+		let copyOfArray = array.slice();
+		copyOfArray[3] = 'changed in copyOfArray';
+		expect(array[3]).toBe('three');
+	});
 });
--- JS-Koans/topics/2 Массивы и объекты/about_this.js
+++ JS-Koans/topics/2 Массивы и объекты/about_this.js
@@ -3,7 +3,7 @@ describe("About this (about_this.js)", function() {
     let person = {
       name: 'bob',
       intro: function () {
-        return "Hello, my name is " + this.FILL_ME_IN;
+        return "Hello, my name is " + this.name;
       }
     }
 
@@ -21,9 +21,9 @@ describe("About this (about_this.js)", function() {
 
     let alias = person.intro;
 
-    // if the function is not called as an object property 'this' is the global context 
+    // if the function is not called as an object property 'this' is the global context
     // (window in a browser). This is an example. Please do not do this in practise.
-    window.FILL_ME_IN = 'Peter';
+    window.globalName = 'Peter';
 
     // What does 'this' refer to when it is not part of an object?
     expect(alias()).toBe("Hello, my name is Peter");
@@ -38,10 +38,9 @@ describe("About this (about_this.js)", function() {
     }
 
     // calling a function with 'call' lets us assign 'this' explicitly
-    let message = person.intro.call({FILL_ME_IN: "Frank"});
+    let message = person.intro.call({name: "Frank"});
 
     // What does 'this' refer to when you use the 'call()' method?
     expect(message).toBe("Hello, my name is Frank");
   });
 });
-
